2841b3530507739437def8565e1c257f8b96d314
Celestial-Fox <stealevw@gmail.com>
2024-12-17 09:53:31 +0100

    03_constuctors_V0.5

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+
+        internal void Run()
+        {
+            QuizVraag quizVraag = new QuizVraag("Hier komt de vraag", "Hier komt het antwoord");
+            Quiz quiz = new Quiz(10,10);
+        }
+    }
+}
---
+++ Constructors/Constructors/Quiz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Quiz
+    {
+        internal QuizVraag[] vragen;
+        internal QuizVraagAntwoord[] ingevuldeAntwoorden;
+
+        internal Quiz(int aantalVragen, int aantalIngevuldeAntwoorden)
+        {
+            vragen = new QuizVraag[aantalVragen];
+            ingevuldeAntwoorden = new QuizVraagAntwoord[aantalIngevuldeAntwoorden];
+        }
+    }
+}
